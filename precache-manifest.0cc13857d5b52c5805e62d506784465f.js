37403624c54f9db0ffad4f845bf4f584dcedc201
YooByungjo <ubj0314@naver.com>
2020-10-02 23:17:20 +0900

    Updates

---
+++ precache-manifest.0cc13857d5b52c5805e62d506784465f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cf6c628ab13ba3886b262641dc6b66",
+    "url": "/twelve-janggi-online-using-Reactjs/index.html"
+  },
+  {
+    "revision": "04cada676e14e7e66fb8",
+    "url": "/twelve-janggi-online-using-Reactjs/static/css/main.1219910b.chunk.css"
+  },
+  {
+    "revision": "9b6f8bfe2976e57416ef",
+    "url": "/twelve-janggi-online-using-Reactjs/static/js/2.4006aa3f.chunk.js"
+  },
+  {
+    "revision": "f5fd50ca0c823a833ca18c96f5035d59",
+    "url": "/twelve-janggi-online-using-Reactjs/static/js/2.4006aa3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04cada676e14e7e66fb8",
+    "url": "/twelve-janggi-online-using-Reactjs/static/js/main.0fee1a5b.chunk.js"
+  },
+  {
+    "revision": "caf9448adfe8179c381a",
+    "url": "/twelve-janggi-online-using-Reactjs/static/js/runtime-main.312ee510.js"
+  }
+]);
